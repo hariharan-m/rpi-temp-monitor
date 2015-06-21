b9cd90d9d41c59cd3a8a3a2408826efe55109c3b
hariharan <hariharan.mathavan@gmail.com>
2015-06-22 00:29:54 +0530

    Minor fixes. Added Icon.

--- app/src/main/java/com/example/hariharan/tempmonitor/MainActivity.java
+++ app/src/main/java/com/example/hariharan/tempmonitor/MainActivity.java
@@ -1,52 +1,40 @@
 package com.example.hariharan.tempmonitor;
 
-import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.PointsGraphSeries;
 import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final String PARSE_APP_ID = "LsrJX7KIwS54yaVaPxQk9KgkLWmlfug0ZAdIQIN6";
+    final String PARSE_CLIENT_KEY = "xgavPnf4nCCOakOSHF6xh5OjVIkO0Bqd0On177SB";
     FloatingActionButton fab;
     GraphView graph;
     View view;
     DataPoint tDataPoints[];
     DataPoint hDataPoints[];
-    LineGraphSeries<DataPoint> tempSeries,humidSeries;
-    final String PARSE_APP_ID = "LsrJX7KIwS54yaVaPxQk9KgkLWmlfug0ZAdIQIN6";
-    final String PARSE_CLIENT_KEY = "xgavPnf4nCCOakOSHF6xh5OjVIkO0Bqd0On177SB";
-
-
+    LineGraphSeries<DataPoint> tempSeries, humidSeries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,13 +50,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
-    protected void initParse()
-    {
+
+    protected void initParse() {
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this,PARSE_APP_ID, PARSE_CLIENT_KEY);
+        Parse.initialize(this, PARSE_APP_ID, PARSE_CLIENT_KEY);
     }
-    protected void initGraphView()
-    {
+
+    protected void initGraphView() {
         graph = (GraphView) findViewById(R.id.graph);
         graph.getLegendRenderer().setVisible(true);
         graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
@@ -104,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-        if (view.getId() == R.id.fab){
+        if (view.getId() == R.id.fab) {
             ParseQuery<ParseObject> query = ParseQuery.getQuery("RpiData");
             query.orderByDescending("createdAt");
             query.setLimit(10);
@@ -124,22 +112,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void setDataToGraph(List<ParseObject> list)
-    {
+    public void setDataToGraph(List<ParseObject> list) {
         tDataPoints = new DataPoint[10];
-        hDataPoints= new DataPoint[10];
-        DataPoint buf1,buf2;
-        graph.setTitle("Updated at "+list.get(0).getCreatedAt());
-        for(int i=0;i<10;i++)
-        {
-            buf1=new DataPoint(i,list.get(9-i).getInt("temp"));
-            buf2=new DataPoint(i,list.get(9-i).getInt("humid"));
-            tDataPoints[i]=buf1;
-            hDataPoints[i]=buf2;
+        hDataPoints = new DataPoint[10];
+        DataPoint buf1, buf2;
+        graph.setTitle("Updated at " + list.get(0).getCreatedAt());
+        for (int i = 0; i < 10; i++) {
+            buf1 = new DataPoint(i, list.get(9 - i).getInt("temp"));
+            buf2 = new DataPoint(i, list.get(9 - i).getInt("humid"));
+            tDataPoints[i] = buf1;
+            hDataPoints[i] = buf2;
 
         }
         tempSeries.resetData(tDataPoints);
         humidSeries.resetData(hDataPoints);
-        Snackbar.make(view, "Latest: Temp:"+list.get(0).getInt("temp")+"°C  Humidity:"+list.get(0).getInt("humid")+"%", Snackbar.LENGTH_LONG).setAction("Hide", this).show();
+        Snackbar.make(view, "Latest: Temp:" + list.get(0).getInt("temp") + "°C  Humidity:" + list.get(0).getInt("humid") + "%", Snackbar.LENGTH_LONG).setAction("Hide", this).show();
     }
 }
